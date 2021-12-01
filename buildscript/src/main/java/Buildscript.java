20fca9f3ee1d2d40e7c31ee863ccb78776c3d72d
IMS212 <ims@ims21.tech>
2021-12-01 13:34:56 -0800

    unpog

--- buildscript/src/main/java/Buildscript.java
+++ buildscript/src/main/java/Buildscript.java
@@ -80,6 +80,7 @@ public class Buildscript extends FabricProject {
     @Override
     public void getModDependencies(ModDependencyCollector d) {
 		d.addMaven("https://repo.maven.apache.org/maven2", new MavenId("org.anarres", "jcpp", "1.4.14"), ModDependencyFlag.COMPILE, ModDependencyFlag.RUNTIME, ModDependencyFlag.JIJ);
+		d.addMaven("https://repo.maven.apache.org/maven2", new MavenId("org.slf4j", "slf4j-api", "1.7.12"), ModDependencyFlag.COMPILE, ModDependencyFlag.RUNTIME, ModDependencyFlag.JIJ);
         d.addMaven(FabricMaven.URL, new MavenId(FabricMaven.GROUP_ID + ".fabric-api", "fabric-resource-loader-v0", "0.4.8+3cc0f0907d"), ModDependencyFlag.COMPILE, ModDependencyFlag.RUNTIME, ModDependencyFlag.JIJ);
         d.addMaven(FabricMaven.URL, new MavenId(FabricMaven.GROUP_ID + ".fabric-api", "fabric-key-binding-api-v1", "1.0.5+3cc0f0907d"), ModDependencyFlag.COMPILE, ModDependencyFlag.RUNTIME, ModDependencyFlag.JIJ);
         if (SODIUM) {
@@ -199,7 +200,8 @@ public class Buildscript extends FabricProject {
                     .vmArgs(
                         "-Dfabric.dli.config=" + launchConfig.toString(),
                         "-Dfabric.dli.env=client",
-                        "-Dfabric.dli.main=net.fabricmc.loader.launch.knot.KnotClient"
+                        "-Dfabric.dli.main=net.fabricmc.loader.launch.knot.KnotClient",
+                        "-XstartOnFirstThread"
                     )
                 .build(),
                 new RunConfigBuilder()
